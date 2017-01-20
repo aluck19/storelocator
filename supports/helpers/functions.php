d0ab26124e20e49d69d27b93162702a94153c166
Abhishek Gupta <abhishek.gupta@deerwalk.edu.np>
2017-01-20 17:33:31 +0545

    email

---
+++ supports/helpers/functions.php
@@ -0,0 +1,31 @@
+<?php
+function email($to, $reply_to_email, $reply_to_name, $subject, $body){
+
+  $mail = new PHPMailer();
+  $mail->IsSMTP();
+  $mail->CharSet="UTF-8";
+  $mail->SMTPSecure = 'tls';
+  $mail->Host = 'smtp.gmail.com';
+  $mail->Port = 587;
+  $mail->Username = 'abhishekguptablog@gmail.com';
+  $mail->Password = '22447435G9';
+  $mail->SMTPAuth = true;
+
+  $mail->FromName = 'StoreMapper by TechLekh.com'; //from
+  $mail->AddAddress($to); //recipients
+  $mail->AddReplyTo($reply_to_email, $reply_to_name); //Name and email of reply to
+  $mail->IsHTML(true);
+  $mail->Subject    = $subject; //subject
+  $mail->AltBody    = "To view the message, please use an HTML compatible email viewer!"; //required in order to read email
+  $mail->Body    = $body; //body
+  if(!$mail->Send())
+  {
+    echo "Mailer Error: " . $mail->ErrorInfo;
+  }
+  else
+  {
+    echo "Message sent!";
+  }
+}
+//emai ('abhishek.gupta@deerwalk.edu.np', 'techlekhnp@gmail.com', 'StoreMapper: TechLekh', 'Check', 'Checked' );
+?>
